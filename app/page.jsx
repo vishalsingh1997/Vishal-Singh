9f9dd49d7c4baf7b8cce0d06b4c62f10d6dc9459
Vishal <vishal1997official@gmail.com>
2024-10-02 13:35:29 +0530

    fourth commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,6 @@
+import Photo from "@/components/Photo";
+import Socials from "@/components/Socials";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -5,21 +8,34 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col lg:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text  */}
           <div className="text-center xl:text-left">
-            <span className="h2">Software Developer</span>
-            <h1 className="h1">
-              Hello I'm <span>Vishal Singh</span>
+            <span className="lg:text-2xl text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /><span className="text-accent">Vishal Singh</span>
             </h1>
-            <p>
+            <p className="max-w-[500px] mb-9 text-white/70">
               I excel at crafting elegant digital experiences and I am
               proficient in various programming languages and technologies.
             </p>
+            {/* button and social icons  */}
+            <div className="flex flex-col items-center gap-8  lg:flex-row">
+              <Button variant="outline" size="lg" className="flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 lg:mb-0">
+                <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center hover:bg-accent text-base hover:text-primary hover:transition-all duration-500" />
+              </div>
+            </div>
           </div>
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
---
+++ components/Stats.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import CountUp from "react-countup"
+
+const stats = [
+    {
+        num: 3,
+        text: "Years of experience",
+    },
+    {
+        num: 12,
+        text: "Project Completed",
+    },
+    {
+        num: 8,
+        text: "Technologies Mastered",
+    },
+    {
+        num: 400,
+        text: "Code Commits",
+    },
+]
+
+const Stats = () => {
+    return (
+        <section className="pt-4 pb-12 lg:pt-0 lg:pb-0">
+            <div className="container mx-auto">
+                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto lg:max-w-none">
+                    {stats.map((item, index) => {
+                        return <div className="flex-1 flex gap-4 items-center justify-center lg:justify-start" key={index}>
+                            <CountUp end={item.num} duration={5} delay={2} className="text-4xl lg:text-6xl font-extrabold" />
+                            <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}> {item.text}</p>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Stats
